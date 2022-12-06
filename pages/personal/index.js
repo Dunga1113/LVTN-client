5c8fa97fc49038aef48d0efd81757434b31b6f50
dung <dunga1113@gmail.com>
2022-12-06 14:35:32 +0700

    d1

--- pages/personal/index.js
+++ pages/personal/index.js
@@ -131,7 +131,7 @@ export default function PersonalPage(props) {
                   label="Họ"
                   id="post-title"
                   variant="filled"
-                  style={{ marginTop: 11, textAlign: "left", width: "48%" }}
+                  style={{ marginTop: 1, textAlign: "left", width: "48%" }}
                   value={userInfo?.first_name}
                   sx={{
                     ".css-10botns-MuiInputBase-input-MuiFilledInput-input": {
